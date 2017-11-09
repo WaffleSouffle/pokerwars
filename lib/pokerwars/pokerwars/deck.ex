7e9f61def5139921f1430960c3eec80b2658152b
Mark English <mark.english@anaplan.com>
2017-11-09 09:13:50 +0000

    me Decks shuffled.

--- lib/pokerwars/pokerwars/deck.ex
+++ lib/pokerwars/pokerwars/deck.ex
@@ -1,10 +1,17 @@
 defmodule Pokerwars.Deck do
   alias Pokerwars.Card
 
-  @initial_deck  (for s <- [:h, :d, :c, :s], n <- 2..14, do: %Card{suit: s, rank: n})
+  @initial_deck  (for s <- [:hearts, :diamonds, :clubs, :spades], n <- 2..14, do: %Card{suit: s, rank: n})
   defstruct [cards: @initial_deck]
 
   def initial_deck do
     @initial_deck
+  end
+
+  def shuffled_deck(opts \\ []) do
+    deck = Keyword.get(opts, :deck, initial_deck())
+    shuffler = Keyword.get(opts, :shuffler, &Enum.shuffle/1)
+
+    shuffler.(deck)
   end
 end
--- test/pokerwars/pokerwars/deck_test.ex
+++ test/pokerwars/pokerwars/deck_test.ex
@@ -1,14 +1,36 @@
 defmodule Pokerwars.DeckTest do
   use ExUnit.Case, async: true
   import Pokerwars.TestHelpers
+  alias Pokerwars.{Deck, Card}
   doctest Pokerwars.Deck
-  
-  test "Deck size" do
-    assert length(%Pokerwars.Deck{}.cards) == 52
+
+  describe "Initial deck" do
+    test "Deck size" do
+      assert length(%Deck{}.cards) == 52
+    end
+
+    test "Initial Deck" do
+      assert length(Deck.initial_deck) == 52
+    end
+
+    test "Initial deck first card is 2 of hearts" do
+      assert Enum.at(Deck.initial_deck, 0) == %Card{suit: :hearts, rank: 2}
+    end
+
+    test "Initial deck last card is 14 of spades" do
+      assert Enum.at(Deck.initial_deck, -1) == %Pokerwars.Card{suit: :spades, rank: 14}
+      #assert Enum.at(Deck.initial_deck, -1) == Pokerwars.Card.parse("As")
+    end
   end
 
-  test "Initial Deck" do
-    assert length(Pokerwars.Deck.initial_deck) == 52
+  describe "Shuffle Deck" do
+    test "Shuffled deck has same size as initial deck" do
+      assert length(Deck.shuffled_deck) == length(Deck.initial_deck)
+    end
+
+    test "Deck shuffled with no-op shuffler is same as initial_deck" do
+      assert Deck.shuffled_deck(shuffler: & &1) == Deck.initial_deck
+    end
   end
 
 end
